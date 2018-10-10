f72d29ef84f3cdc200d03a972914594beff1c3b1
hebidu <hebiduhebi@126.com>
2018-10-10 16:05:26 +0800

    【更新】增加计算 2个经纬度之间的距离

---
+++ src/geo/MathHelper.php
@@ -0,0 +1,37 @@
+<?php
+// +----------------------------------------------------------------------
+// |
+// +----------------------------------------------------------------------
+// | ©2018 California State Lottery All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Smith Jack
+// +----------------------------------------------------------------------
+
+namespace by\component\geo;
+
+
+class MathHelper
+{
+    /**
+     * 求两个已知经纬度之间的距离,单位为米
+     *
+     * @param $lng1
+     * @param $lat1
+     * @param $lng2
+     * @param $lat2
+     * @return float 距离，单位米
+     */
+    function getDistanceOf($lng1, $lat1, $lng2, $lat2)
+    {
+        // 将角度转为狐度
+        $radLat1 = deg2rad($lat1); //deg2rad()函数将角度转换为弧度
+        $radLat2 = deg2rad($lat2);
+        $radLng1 = deg2rad($lng1);
+        $radLng2 = deg2rad($lng2);
+        $a = $radLat1 - $radLat2;
+        $b = $radLng1 - $radLng2;
+        $s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6378.137 * 1000;
+        return $s;
+    }
+
+}
